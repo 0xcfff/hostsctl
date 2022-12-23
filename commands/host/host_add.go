19d58b0806164e8eb0832dffe43e7c8a9f76f5a5
Mikhail <0xcfff@gmail.com>
2022-12-24 02:24:47 +0400

    Remove unneeded code

--- commands/host/host_add.go
+++ commands/host/host_add.go
@@ -3,7 +3,6 @@ package host
 import (
 	"fmt"
 
-	"github.com/0xcfff/hostsctl/backend/hosts"
 	"github.com/spf13/cobra"
 )
 
@@ -13,16 +12,16 @@ func NewCmdHostAdd() *cobra.Command {
 		Use:   "add [ip] [alias]...",
 		Short: "Adds IP address and aliases to /etc/hosts file",
 		Run: func(cmd *cobra.Command, args []string) {
-			ip := &hosts.IPRecord{
-				IP:      args[0],
-				Aliases: args[1:],
-			}
-			fs := hosts.NewHostsFileSource("", nil)
-			f, err := fs.LoadFile()
-			cobra.CheckErr(err)
+			// ip := &hosts.IPRecord{
+			// 	IP:      args[0],
+			// 	Aliases: args[1:],
+			// }
+			// fs := hosts.NewHostsFileSource("", nil)
+			// f, err := fs.LoadFile()
+			// cobra.CheckErr(err)
 
-			f.AppendIp(ip)
-			f.Dump()
+			// f.AppendIp(ip)
+			// f.Dump()
 
 			cobra.CheckErr(fmt.Errorf("Not Implemented"))
 		},
