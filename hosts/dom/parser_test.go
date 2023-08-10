f7eefb3cf4f2f3ba9f700564f1e9b826cd21735e
Mikhail <0xcfff@gmail.com>
2023-08-10 09:41:29 +0400

    Add test for unrecognized lines

--- hosts/dom/parser_test.go
+++ hosts/dom/parser_test.go
@@ -60,6 +60,25 @@ func Test_parse(t *testing.T) {
 		assert.Equal(t, "proj-01", b0.Name())
 		assert.Equal(t, "system ips", b0.Note())
 	})
+	t.Run("unrecognized lines", func(t *testing.T) {
+		content := `unrecognized line 1
+# comment
+unrecognized line 2`
+		syndoc, _ := syntax.Read(strings.NewReader(content))
+		doc := parse(syndoc)
+
+		assert.Equal(t, syndoc, doc.originalDocument)
+		assert.Equal(t, 3, doc.BlocksCount())
+		assert.Equal(t, Unknown, doc.Blocks()[0].Type())
+		b0 := doc.Blocks()[0].(*UnrecognizedBlock)
+		assert.Equal(t, 1, len(b0.BodyElements()))
+		assert.Equal(t, Comments, doc.Blocks()[1].Type())
+		b1 := doc.Blocks()[1].(*CommentsBlock)
+		assert.Equal(t, 1, len(b1.origComments))
+		assert.Equal(t, Unknown, doc.Blocks()[2].Type())
+		b2 := doc.Blocks()[2].(*UnrecognizedBlock)
+		assert.Equal(t, 1, len(b2.BodyElements()))
+	})
 	t.Run("empty ip block", func(t *testing.T) {
 		content := `# custom ips
 # <<placeholder>>`
