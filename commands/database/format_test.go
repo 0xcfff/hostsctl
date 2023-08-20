039002f99e122e9b36f6b419f0cf9477a09013ec
Mikhail <0xcfff@gmail.com>
2023-08-20 09:04:02 +0400

    Add a print test

--- commands/database/format_test.go
+++ commands/database/format_test.go
@@ -12,7 +12,7 @@ func TestDatabaseFormatCommand(t *testing.T) {
 		{
 			Name: "format - empty",
 			Args: cmdtest.ITArgs{
-				Args:       []string{"15"},
+				Args:       []string{},
 				InputFile:  "testdata/empty.txt",
 				OutputFile: "testdata/format/format__empty__result.txt",
 			},
@@ -21,9 +21,18 @@ func TestDatabaseFormatCommand(t *testing.T) {
 		{
 			Name: "format - non empty",
 			Args: cmdtest.ITArgs{
-				Args:       []string{"15"},
+				Args:       []string{},
 				InputFile:  "testdata/six-blocks.txt",
 				OutputFile: "testdata/format/format__non_empty__result.txt",
+			},
+			Want: true,
+		},
+		{
+			Name: "format dry run - non empty",
+			Args: cmdtest.ITArgs{
+				Args:       []string{"--dry-run"},
+				InputFile:  "testdata/six-blocks.txt",
+				StdoutFile: "testdata/format/format_dry_run__non_empty__output.txt",
 			},
 			Want: true,
 		},
